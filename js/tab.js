e522e2cfda87f5746c6d7cfbff38dd07601c8562
wangxiaoniu <13519870949@163.com>
2017-01-17 17:32:53 +0800

    ban

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+	// 头部导航选中切换
  $(".nav").find("li").click(function(){
             $(this).addClass("active").siblings("li").removeClass("active");
+        });
+
+ // main导航选中状态切换
+
+  $('.nav_tab li a').click(function(){
+        	$(this).addClass("active");
+        	$(this).parent("li").siblings("li").find("a").removeClass("active");
+        	var index = $(this).index();
+        	$('.main_content').eq(index).show().addClass('active').siblings().removeClass('active').hide();
         });
 });
